7547a408730a07905cefb879c264b5f06873c55b
josuegerardo96 <josuegerardo09@gmail.com>
2021-04-21 18:13:02 -0600

    Intento de diseño de matriz

--- helpersWindow.rkt
+++ helpersWindow.rkt
@@ -38,61 +38,45 @@
 
 
 
-;Intento de mostrar el A
-(define (A-star inicio meta pos)
-  (local [(define (get-path rsf)
-            (if (false? rsf)
-                false
-                (reverse (map (λ (p) (wle-pos p)) rsf))))
-       
-        
-          (define (pos-member? pos lop)
-            (cond [(empty? lop) false]
-                  [else
-                   (if(meta? pos (first lop))
-                      true
-                      (pos-member? pos (rest lop)))]))
+(define (show c r)
+  (cond
+    ((= r 0) scn)
+    (else (let loop_r ((j 0)) (cond
+                                ((= j r) scn)
+                                (else (let loop_c ((i 0)) (cond
+                                                            ((= i c) (loop_r (add1 j)))
+                                                            (else (begin
 
-          (define (neighbour p pos)
-            (local [(define x (pos-x p))
-                    (define y (pos-y p))
-                    ;; determine if n is in the board pos
-                    (define (in? n) 
-                      (local [(define (in-board? n pos)
-                                (cond [(empty? pos) false]
-                                      [else
-                                       (if (meta? n (first pos))
-                                           true
-                                           (in-board? n (rest pos)))]))]
-                        (in-board? n pos)))]
-              (filter in? 
-                      (list 
-                       (make-pos (add1 x) y)
-                       (make-pos (sub1 x) y)
-                       (make-pos x (add1 y))
-                       (make-pos x (sub1 y))))))
-          
-          (define (remove-first-n lst n)
-            (cond [(or (zero? n) (empty? lst)) lst]
-                  [else
-                   (remove-first-n (rest lst) (sub1 n))]))
-    
+                                                                    (if (= (list-ref index_list (+ i (* j rows))) current) (begin
+                                                                                                                             (if (>= (- i 1) 0)        (set! neighbours_visited_list (list-set neighbours_visited_list 3 (list-ref visited_list (+ (- i 1) (* j rows))))) (set! neighbours_visited_list (list-set neighbours_visited_list 3 -1)))
+                                                                                                                             (if (<= (+ i 1) (- c 1))  (set! neighbours_visited_list (list-set neighbours_visited_list 1 (list-ref visited_list (+ (+ i 1) (* j rows))))) (set! neighbours_visited_list (list-set neighbours_visited_list 1 -1)))
+                                                                                                                             (if (>= (- j 1) 0)        (set! neighbours_visited_list (list-set neighbours_visited_list 0 (list-ref visited_list (+ i (* (- j 1) rows))))) (set! neighbours_visited_list (list-set neighbours_visited_list 0 -1)))
+                                                                                                                             (if (<= (+ j 1) (- r 1))  (set! neighbours_visited_list (list-set neighbours_visited_list 2 (list-ref visited_list (+ i (* (+ j 1) rows))))) (set! neighbours_visited_list (list-set neighbours_visited_list 2 -1)))
+                                                                                                                             ) void)
+
+                                                                    (place-images
+
+                                                                   
+                                                                     (list
+                                                                    
+                                                                      (rectangle scl 2 "solid" (color 255 255 255 (if (= (list-ref (list-ref wall_list (+ i (* j rows))) 0) 1) 255 0))) ; TOP
+                                                                      (rectangle scl 2 "solid" (color 255 255 255 (if (= (list-ref (list-ref wall_list (+ i (* j rows))) 2) 1) 255 0))) ; BOTTOM
+                                                                      (rectangle 2 scl "solid" (color 255 255 255 (if (= (list-ref (list-ref wall_list (+ i (* j rows))) 3) 1) 255 0))) ; LEFT
+                                                                      (rectangle 2 scl "solid" (color 255 255 255 (if (= (list-ref (list-ref wall_list (+ i (* j rows))) 1) 1) 255 0))) ; RIGHT
+                                                                      (rectangle scl scl "solid" (if (= (list-ref index_list (+ i (* j rows))) current) (color 200 0 200 255) (color 180 0 150 (if (= (list-ref visited_list (+ i (* j rows))) 1) 255 0))))
+
+                                                                      )
 
 
+                                                                     (list
+                                                                    
+                                                                      (make-posn (+ (/ scl 2) (* i scl))  (* j scl))               ; TOP
+                                                                      (make-posn (+ (/ scl 2) (* i scl))  (+ scl (* j scl)))       ; BOTTOM
+                                                                      (make-posn (* i scl)                (+ (/ scl 2) (* j scl))) ; LEFT
+                                                                      (make-posn (+ scl (* i scl))        (+ (/ scl 2) (* j scl))) ; RIGHT
+                                                                      (make-posn (+ (/ scl 2) (* i scl))  (+ (/ scl 2) (* j scl)))
 
-
-
-
-(define frame (new frame%
-                   [label "MAZE"]
-                   [min-width 500]
-                   [min-height 500]
-                   [stretchable-width #f]
-                   [stretchable-height #f]))
-(define canvas (new canvas%
-                    [parent frame]
-                    [paint-callback	(lambda (canvas dc)
-                                          (set-for-each MAZES (curry draw-maze dc)))]))
-(send canvas set-canvas-background (make-object color% "darkred"))
-(send frame show #t)
-
+                                                                      ) (loop_c (add1 i)))))
+                                                            )))
+                                ))
+          )))
